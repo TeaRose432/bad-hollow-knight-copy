052ea191ee041ee41eccc254c9d4dc0b007d9b05
TeaRose432 <cds4498@hotmail.com>
2025-12-01 09:25:59 -0600

    updated proposal md

--- proposal.md
+++ proposal.md
@@ -4,13 +4,19 @@
 - A bad copy of the game Hollow Knight
 
 ## Needs to have
-- players and enemies
+- players
+    - a choice between two characters with different strengths
+- enemies
+    - enemies to fight while exploring, like a jumping enemy, a charging one. Different health and damage depending on the enemy.
 - multiple rooms to explore
 - attack animations/ability to kill enemies
 - 3 lives/hits mechanic
+    - if hit three times you "die" and must respawn
+    - checkpoints to respawn at
 
 ## Nice to have
 - a puzzle or two while exploring
+    - a puzzle that needs to be done to unlock the next room
 - animation for player gettings hit
-- a boss or two to fight
+- a boss or two to fight, you discover them while exploring
 - be behind foreground
--- sketch.js
+++ sketch.js
@@ -5,6 +5,35 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let lgStand;//setting up character movement and idle animations
+let lgStandRight;
+let lgRun;
+let lgRunRight;
+let hornetIdle;
+let horentIdleRight;
+let hornetRun;
+let hornetRunRight;
+let direction = "right";//is used to know which way character should face when movement stops
+
+function preload() {//loading images and animations
+  lilGuyImg = loadImage("images-folder/lil-guy.png");
+  hornetImg = loadImage("images-folder/hornet.png");
+  lgStand = loadImage("images-folder/ghost-standing.gif");
+  lgStandRight = loadImage("images-folder/ghost-standing-right.gif");
+  lgRun = loadImage("images-folder/hollow-knight-walk.gif");
+  lgRunRight = loadImage("images-folder/hollow-knight-walk-right.gif");
+  hornetRun = loadImage("images-folder/hornet-run.gif");
+  hornetRunRight = loadImage("images-folder/hornet-run-right.gif");
+  hornetIdle = loadImage("images-folder/hornet-idle.webp");
+  hornetIdleRight = loadImage("images-folder/hornet-idle-right.webp");
+  enemyBG = loadImage("images-folder/big-guy-walk.gif");
+  enemyJ = loadImage("images-folder/jumper-walk.gif");
+  enemyC = loadImage("images-folder/charger-walk.gif");
+  enemyBGRight = loadImage("images-folder/big-guy-walk-right.gif");
+  enemyJRight = loadImage("images-folder/jumper-walk-right.gif");
+  enemyCRight = loadImage("images-folder/charger-walk-right.gif");
+}
+
 class Player {
   constructor(x, y) {
     this.pX = x;
@@ -27,6 +56,8 @@ class Player {
     
   }
 }
+
+let lilGuy;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
